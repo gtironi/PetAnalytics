a4e66dd8482e336e9f8fc212ee068c5cbdedf803
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-01-13 15:56:37 -0300

    I've created the .h file that defines the IMUClass
    - Created IMU.h with IMUClass definition
    - Added methods for sensor initialization (begin) and raw data reading (readRawData)
    - Included necessary libraries: Arduino.h, ICM_20948.h, and SPI.h

---
+++ IMU.h
@@ -0,0 +1,21 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include "Arduino.h"
+#include "ICM_20948.h"
+#include <SPI.h>
+
+class IMUClass {
+public:
+  IMUClass(uint8_t csPin);
+  void begin();
+  void readRawData();
+
+private:
+  uint8_t csPin;
+  ICM_20948_SPI imu;
+  void printRawAGMT(ICM_20948_AGMT_t agmt);
+  void printPaddedInt16b(int16_t val);
+};
+
+#endif
